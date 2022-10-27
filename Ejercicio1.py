24d3a043971584a55e4a53e92f77e33f5718c96f
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-10-27 10:33:46 +0200

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+'''
+PREGUNTA 1
+
+Realiza los siguientes ejercicios en un fichero llamado Ejercicio1.py:
+*Convertir la variable "var_1" en todas las letras mayúsculas y en minúsculas (var_1 = "Módulo 1 de Python ")
+*Consulta el tamaño o la longitud de la variable "var_1" y calcula la división de ese numero entre "7" redondeado a 4 decimales
+*Realiza una función llamada funcion1  que calcule siguiente operación para que el resultado final sea igual a cero (0) //12 - (4 * 2) - (2 + 2)
+*Realiza una función llamada funcion2 para que calcule la siguiente operación para que el resultado final sea igual a catorce (14)// 12 - 4 * (2 - 2) + 2
+*Realiza una función llamda funcion3 para pedir al usuario que introduzca su edad, y después imprimir en la pantalla si es meyor de edad o no
+Adjuntar archivo  
+'''
+
+
