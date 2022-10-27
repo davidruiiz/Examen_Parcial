0f80189d21b2b3dcb6c9e0bd228364821dcf3b43
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-10-27 11:41:41 +0200

    Update multiplicador.py

--- multiplicador.py
+++ multiplicador.py
@@ -9,3 +9,12 @@ Multiplica el numero_magico por el numero_usuario en sí mismo
 Finalmente muestra el valor final del numero_magico por pantalla
 '''
 
+numero_magico=12345679
+try:
+    numero_usuario=int(input('Introduce un número del 1 al 9:\n'))
+    numero_usuario*=9
+    numero_magico*=numero_usuario
+    print(numero_magico)
+except ValueError:
+    print('Debe ser un número entero. Prueba otra vez.')
+
