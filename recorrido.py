a2cfcca7138bce3ab6b4236abdac69e9bc129dfc
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-10-27 11:29:16 +0200

    Update recorrido.py

--- recorrido.py
+++ recorrido.py
@@ -9,7 +9,8 @@ Parar el programa si llega a un número mayor que 300
 
 lista=[18, 50, 210, 80, 145, 333, 70, 30]
 for index in lista:
-    if index%10==0:
-        print('{} es múltiplo de 10.'.format(index))
-        
+   if index%10==0 and index<200:
+        print('{} es múltiplo de 10 y menor que 200.'.format(index))
+
+
 
